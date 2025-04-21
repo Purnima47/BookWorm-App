7f69bcf5799a1c8c9533790bfd7aa242f312642f
Purnima Kesarwani <purnimakesarwani47@gmail.com>
2025-04-21 16:48:04 +0530

    BookWorm

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -39,7 +39,7 @@ router.post("/register", async (req, res) => {
         }
 
         // get random avatar
-        const profileImage = `https://api.dicebear.com/7.x/avataars/svg?seed=${username}`
+        const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`
 
         const user = new User({
             email,
